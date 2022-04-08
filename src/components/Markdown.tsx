1213707b62c6dcd3057765706fc6f7f71b407d49
Guilherme Ananias <iamguilhermeananias@gmail.com>
2022-04-08 01:57:37 -0300

    add react-syntax-highlither on Markdown component

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,4 +1,6 @@
 import ReactMarkdown, { Components } from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { prism } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import rehypeKatex from 'rehype-katex';
 import remarkGfm from 'remark-gfm';
 import remarkMath from 'remark-math';
@@ -9,9 +11,27 @@ import 'katex/dist/katex.min.css';
 import { theme } from '../theme';
 
 const defaultComponents: Components = {
-  h1: ({ children, level }) => (
-    <h2>{children}</h2>
-  ),
+  h1: ({ children }) => <h2>{children}</h2>,
+  h2: ({ children }) => <h3>{children}</h3>,
+  h3: ({ children }) => <h4>{children}</h4>,
+  code: ({ node, inline, className, children, lang, ...props }) => {
+    const match = /language-(\w+)/.exec(className || '');
+
+    return !inline && match ?
+      (
+        <SyntaxHighlighter
+          style={prism}
+          language={match[1]}
+
+        >
+          {String(children).replace(/\n$/, '')}
+        </SyntaxHighlighter>
+      ) : (
+        <code {...{ className, ...props }}>
+          {children}
+        </code>
+      )
+  },
 }
 
 type Props = {
