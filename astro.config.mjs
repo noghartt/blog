559ef43e02d071d8190cc798aea663ae3d0ea9e7
Guilherme Ananias <hi@noghartt.dev>
2023-02-05 22:01:42 -0300

    feat(astro): setup vercel and mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  output: "server",
+  adapter: vercel()
 });
