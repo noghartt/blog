219ac8d2b6ccf728a657536f12e43030d3df3119
Guilherme Ananias <hi@noghartt.dev>
2023-02-05 22:05:51 -0300

    build(vercel): replace serverless by static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
