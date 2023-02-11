31c7e0a351ddfe25dfef8054f09c4261f72c9aa8
Guilherme Ananias <hi@noghartt.dev>
2023-02-10 22:59:53 -0300

    feat(rss): add content tag to rss item

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,7 +1,11 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
 
 import { SITE } from "../config";
+
+const parser = new MarkdownIt();
 
 export async function get() {
   const posts = await getCollection("blog", ({ data }) => !data.draft);
@@ -10,11 +14,12 @@ export async function get() {
     title: SITE.title,
     description: SITE.description,
     site: SITE.website,
-    items: posts.map(({ data, slug }) => ({
+    items: posts.map(({ data, slug, body }) => ({
       link: slug,
       title: data.title,
       description: data.description,
       pubDate: data.pubDate,
+      content: sanitizeHtml(parser.render(body))
     })),
   });
 }
