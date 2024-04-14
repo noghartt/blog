6e764b860479b1f9eb92f67586dbc50f20b48b17
Guilherme Ananias <hi@noghartt.dev>
2024-04-13 22:00:18 -0300

    feat(rss): sort rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -10,16 +10,20 @@ const parser = new MarkdownIt();
 export async function GET() {
   const posts = await getCollection("blog", ({ data }) => !data.draft);
 
-  return rss({
-    title: SITE.title,
-    description: SITE.description,
-    site: SITE.website,
-    items: posts.map(({ data, slug, body }) => ({
+  const items = posts
+    .sort((a, b) => new Date(b.data.pubDate) - new Date(a.data.pubDate))
+    .map(({ data, slug, body }) => ({
       link: slug,
       title: data.title,
       description: data.description,
       pubDate: data.pubDate,
       content: sanitizeHtml(parser.render(body))
-    })),
+    }));
+
+  return rss({
+    title: SITE.title,
+    description: SITE.description,
+    site: SITE.website,
+    items,
   });
 }
