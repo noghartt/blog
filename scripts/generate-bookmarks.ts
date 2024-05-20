1f9acf21f44914a31abd4a951dc37e639e12411a
Guilherme Ananias <hi@noghartt.dev>
2024-05-20 08:46:02 -0300

    feat(blog): bring static bookmark page

---
+++ scripts/generate-bookmarks.ts
@@ -0,0 +1,102 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const OMNIVORE_API_URL = 'https://api-prod.omnivore.app/api/graphql';
+const OMNIVORE_API_KEY = process.env.OMNIVORE_API_KEY;
+
+const query = `\
+  query SearchBookmarks($first: Int, $after: String, $query: String) {
+    search(first: $first, query: $query, after: $after) {
+      ... on SearchSuccess {
+        pageInfo {
+          hasNextPage
+          endCursor
+          totalCount
+        }
+        edges {
+          cursor
+          node {
+            id
+            title
+            url
+            savedAt
+          }
+        }
+      }
+
+      ... on SearchError {
+        errorCodes
+      }
+    }
+  }
+`;
+
+const fetchBookmarks = async (first: number, after: string) => {
+  console.log('Fetching bookmarks:', { first, after })
+
+  try {
+    const response = await fetch(OMNIVORE_API_URL, {
+      method: 'POST',
+      body: JSON.stringify({ query, variables: { first, after, query: "no:subscription" } }),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: OMNIVORE_API_KEY,
+      },
+    });
+
+    if (!response.ok) {
+      return { error: response.statusText, data: null };
+    }
+
+    return {
+      error: null,
+      data: await response.json(),
+    }
+  } catch (err) {
+    return { error: err, data: null };
+  }
+}
+
+const writeJsonFile = async (data: any) => {
+  const cwd = process.cwd();
+  const filepath = path.join(cwd, 'src', 'pages', '_bookmarks.json');
+  await fs.writeFile(filepath, JSON.stringify({ lastUpdate: new Date().toISOString(), data }, null, 2));
+}
+
+(async () => {
+  const bookmarks = await fetchBookmarks(1000, null);
+  if (bookmarks.error) {
+    console.log(bookmarks.error);
+    return;
+  }
+
+  const { edges } = bookmarks.data.data.search;
+
+  const bookmarksList = edges.map(({ node }) => ({
+    id: node.id,
+    title: node.title,
+    url: node.url,
+    savedAt: node.savedAt,
+  }));
+
+  let after = edges[edges.length - 1].cursor;
+  let hasNextPage = bookmarks.data.data.search.pageInfo.hasNextPage;
+  while (hasNextPage) {
+    const nextBookmarks = await fetchBookmarks(1000, after);
+    if (nextBookmarks.error) {
+      console.log(nextBookmarks.error);
+      return;
+    }
+
+    after = nextBookmarks.data.data.search.pageInfo.endCursor;
+    hasNextPage = nextBookmarks.data.data.search.pageInfo.hasNextPage;
+    bookmarksList.push(...nextBookmarks.data.data.search.edges.map(({ node }) => ({
+      id: node.id,
+      title: node.title,
+      url: node.url,
+      savedAt: node.savedAt,
+    })));
+  }
+
+  await writeJsonFile(bookmarksList);
+})();
