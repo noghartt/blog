94b00fa8d9d47a89a7999232c141b8fbe6b007c2
Guilherme Ananias <hi@noghartt.dev>
2024-06-25 22:44:20 -0300

    feat(blog): simplify url

--- plugins/rehypePluginLinkHeading.ts
+++ plugins/rehypePluginLinkHeading.ts
@@ -3,10 +3,6 @@ import { visit } from 'unist-util-visit';
 import { modifyChildren } from 'unist-util-modify-children';
 import slugify from 'slugify';
 
-const BASE_URL = process.env.NODE_ENV === 'development'
-  ? 'http://localhost:4321'
-  : 'https://noghartt.dev';
-
 const modifyHeading = (url: string) => modifyChildren((node) => {
   if (node.type !== 'text') {
     return;
@@ -30,16 +26,13 @@ const modifyHeading = (url: string) => modifyChildren((node) => {
 })
 
 export function rehypePluginLinkHeading() {
-  return function(tree: Root, { history }) {
-    const contentPath = history[0].split('/content')[1].replace('.md', '');
-    const url = BASE_URL + contentPath;
+  return function(tree: Root) {
     visit(tree, 'element', (node) => {
       const headingElements = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
       if (headingElements.includes(node.tagName)) {
         const title = node.children.map((child) => child.value).join('');
         const slug = slugify(title, { lower: true, replacement: '-', strict: true });
-        const hashedUrl = `${url}#${slug}`;
-        modifyHeading(hashedUrl)(node);
+        modifyHeading(`#${slug}`)(node);
       }
     });
   }
