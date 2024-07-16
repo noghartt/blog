bacf4ac020763b3a2936e85a19ee0ce2327819cd
Guilherme Ananias <hi@noghartt.dev>
2024-07-16 11:59:10 -0300

    feat(blog): fix blog schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,14 +2,20 @@ import { defineCollection, z } from 'astro:content';
 import { rssSchema } from '@astrojs/rss';
 
 const blogCollection = defineCollection({
+  type: 'content',
   schema: z
     .object({
+      slug: z.string().optional(),
+      title: z.string(),
+      pubDate: z.coerce.date(),
       tags: z.optional(z.array(z.string())),
+      draft: z.boolean().optional(),
     })
     .merge(rssSchema)
 });
 
 const listsCollection = defineCollection({
+  type: 'content',
   schema: z
     .object({
       tags: z.optional(z.array(z.string())),
@@ -17,7 +23,7 @@ const listsCollection = defineCollection({
     .merge(rssSchema)
 });
 
-export const collection = {
+export const collections = {
   blog: blogCollection,
   lists: listsCollection,
 }
