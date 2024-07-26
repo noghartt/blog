14ced4809cd45fddb5a1f2204879491a34e0cf96
Guilherme Ananias <hi@noghartt.dev>
2024-07-26 00:42:26 -0300

    feat(blog): add sidenotes support

--- astro.config.ts
+++ astro.config.ts
@@ -7,34 +7,26 @@ import rehypePrettyCode from 'rehype-pretty-code';
 import fs from 'fs/promises';
 import graymatter from 'gray-matter';
 import path from 'path';
-
 import { rehypePluginLinkHeading } from './plugins/rehypePluginLinkHeading';
 import { rehypePluginTableWrapper } from './plugins/rehypePluginTableWrapper';
 import { remarkPluginReadingTime } from './plugins/remarkPluginReadingTime';
-
+import mdx from "@astrojs/mdx";
 const BLOG_DIR = './src/content/blog';
-
 const getBlogRoutesRedirect = async () => {
   const blogRoutesOldSlug = await fs.readdir(BLOG_DIR);
-  const blogRoutes = blogRoutesOldSlug
-    .map(post => {
-      const frontmatter = graymatter.read(path.join(BLOG_DIR, post));
-      return {
-        ...frontmatter,
-        slug: frontmatter.data.slug,
-      }
-    })
-    .map(({ slug }) => [
-      `/${slug}`,
-      `/blog/${slug}`,
-    ]);
-
+  const blogRoutes = blogRoutesOldSlug.map(post => {
+    const frontmatter = graymatter.read(path.join(BLOG_DIR, post));
+    return {
+      ...frontmatter,
+      slug: frontmatter.data.slug
+    };
+  }).map(({
+    slug
+  }) => [`/${slug}`, `/blog/${slug}`]);
   return Object.fromEntries(blogRoutes);
-}
+};
+const disableSitemap = ['/blog/drafts'];
 
-const disableSitemap = [
-  '/blog/drafts',
-];
 
 // https://astro.build/config
 export default defineConfig({
@@ -52,6 +44,7 @@ export default defineConfig({
       }
     }),
     react(),
+    mdx(),
   ],
   output: "static",
   markdown: {
