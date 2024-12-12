501750831d6bab461c9fc6161a48b6d42b680734
Guilherme Ananias <iamguilhermeananias@gmail.com>
2024-12-12 01:43:08 -0300

    add flake on blog

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Blog Flakefile";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          packages = [
+            nodejs_23
+            nodePackages.yarn
+            pnpm
+            yarn
+          ];
+        };
+      }
+    );
+}
